c2963191f7ca4bbca2f0895e6a1f1e48a8164264
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2020-05-10 10:32:29 +0100

    for loop

--- script.js
+++ script.js
@@ -18,3 +18,22 @@ while (index < length) {
 }
 
 console.log(prices);
+
+
+var users = [
+    { username: 'asdfasdf', credits: 150 },
+    { username: 'asasdfdf', credits: 510 },
+    { username: 'asdasdffasdf', credits: 11 },
+    { username: 'weewrwe', credits: 0 },
+    { username: 'werwer', credits: 120 },
+    { username: 'sdfsdf', credits: 130 },
+    { username: 'dfgdfg', credits: 0 },
+    { username: 'dfgdfg', credits: 510 },
+    { username: '4564564', credits: 110 },
+];
+
+//Iterate through the array using a for loop and add 10 credits to all users.
+
+for (var us = 0; us < users.length; us++) {
+    users[us].credits += 10;
+}
