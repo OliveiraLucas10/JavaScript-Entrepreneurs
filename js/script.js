a797ba24e1b81ec2b119737241f565af6f9fbaa0
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2020-05-10 14:21:23 +0100

    getting input values

--- js/script.js
+++ js/script.js
@@ -5,8 +5,22 @@ window.addEventListener("load", function() {
     this.console.log(searchBtn);
 
     function search() {
+        // get hobby
+        var hobbyField = document.getElementById("hobby");
+        var hobby = hobbyField.value;
+        console.log(hobby);
+
+        // get gender
+        var genderField = document.getElementById("gender");
+        var s = genderField.selectedIndex;
+        var gender = genderField.options[s].value;
+        console.log(gender);
+
+
         console.log("The button was clicked!");
     }
 
     searchBtn.addEventListener("click", search);
+
+
 });
