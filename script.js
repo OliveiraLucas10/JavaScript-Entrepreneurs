fce14ed3ab39d0345ed3ae2348bdd099cf4fecc9
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2020-05-10 10:16:39 +0100

    added simple while loop on array

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ function converter(celcius) {
 }
 
 converter(30);
+
+
+var prices = [10, 15, 25, 8, 4, 55, 99, 11, 15, 25, 5, 4, 65, 5, 10, 15, 7, 8, 4, 9, 100];
+
+//using a while loop, traverse the array and multiply each price by 2
+var index = 0,
+    length = prices.length;
+while (index < length) {
+    prices[index] *= 2;
+    index++;
+}
+
+console.log(prices);
